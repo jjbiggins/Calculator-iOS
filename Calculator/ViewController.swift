c1f040d7dca2a4d394311056f0fd988ebfd4dc4b
Joe Biggins <joseph-biggins@uiowa.edu>
2019-03-12 05:54:44 -0500

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Joe Biggins on 3/12/19.
+//  Copyright © 2019 Joe Biggins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
